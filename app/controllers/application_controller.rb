3b4ba4613a5b473fe012eca7e85d898319ab3f39
sophiafan <172404042@qq.com>
2019-04-08 17:35:13 +0800

    finish devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_customer!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :wechat_id])
+
+  end
 end
