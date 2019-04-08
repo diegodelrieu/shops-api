e12ced52cb0df879ffbc7386d819152fad12f2b1
diego delrieu <diego.delrieu@gmail.com>
2019-04-08 17:35:27 +0800

    Fixed several issues, updated the seed, created the view

--- app/views/api/v1/shops/show.json.jbuilder
+++ app/views/api/v1/shops/show.json.jbuilder
@@ -1,4 +1,4 @@
 json.extract! @shop, :id, :name, :address, :avatar_url, :description, :opening_hours, :rating_from_diaping
-json.goods @restaurant.goods do |good|
-  json.extract! good, :id, :description, :name, :original_price, :discount_price, :quantity, :expiry_time
+json.goods @shop.items do |item|
+  json.extract! item, :id, :description, :name, :original_price, :discount_price, :quantity, :expiry_time
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_064206) do
+ActiveRecord::Schema.define(version: 2019_04_08_093111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,33 @@ ActiveRecord::Schema.define(version: 2019_04_08_064206) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "goods", force: :cascade do |t|
+    t.bigint "shop_id"
+    t.string "name"
+    t.string "description"
+    t.string "original_price"
+    t.string "discount_price"
+    t.integer "quantity"
+    t.string "expiry_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_goods_on_shop_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.bigint "shop_id"
+    t.string "name"
+    t.string "description"
+    t.string "original_price"
+    t.string "discount_price"
+    t.integer "quantity"
+    t.string "expiry_time"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_items_on_shop_id"
   end
 
   create_table "shops", force: :cascade do |t|
@@ -34,4 +61,6 @@ ActiveRecord::Schema.define(version: 2019_04_08_064206) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "goods", "shops"
+  add_foreign_key "items", "shops"
 end
