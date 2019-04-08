9623835b6c34368e3ba38edddbd7c2a0266652a5
sophiafan <172404042@qq.com>
2019-04-09 01:04:17 +0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :sessions, only: [:create]
       resources :shops, only: [ :index, :show, :new, :create ]
     end
   end
