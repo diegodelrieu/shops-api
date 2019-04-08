f5d0160c22750898c8fe7be34904f95a890dd45a
sophiafan <172404042@qq.com>
2019-04-08 18:57:43 +0800

    added show skip

--- app/controllers/api/v1/shops_controller.rb
+++ app/controllers/api/v1/shops_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ShopsController < Api::V1::BaseController
 
   before_action :set_shop, only: [:show]
-  skip_before_action :authenticate_customer!, only: :index
+  skip_before_action :authenticate_customer!, only: [:index, :show]
 
   def index
     @shops = Shop.all
