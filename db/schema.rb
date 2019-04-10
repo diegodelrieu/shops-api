86c56470657e990d9c67583ae8e936e4adc22112
diego delrieu <diego.delrieu@gmail.com>
2019-04-10 16:15:18 +0800

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_031207) do
+ActiveRecord::Schema.define(version: 2019_04_10_074444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,9 +60,9 @@ ActiveRecord::Schema.define(version: 2019_04_10_031207) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "authentication_token", limit: 30
-    t.integer "longitude"
-    t.integer "latitude"
     t.string "phone_number"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["authentication_token"], name: "index_shops_on_authentication_token", unique: true
     t.index ["email"], name: "index_shops_on_email", unique: true
     t.index ["reset_password_token"], name: "index_shops_on_reset_password_token", unique: true
