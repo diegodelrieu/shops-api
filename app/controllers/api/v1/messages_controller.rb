bd7cbb5bcee2befa6842c1c23a9b23e6d55a0d26
sophiafan <172404042@qq.com>
2019-04-16 20:59:24 +0800

    bug

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < Api::V1::BaseController
   respond_to :json
-  skip_before_action :authenticate_customer!
+  skip_before_action :authenticate_customer!, only: [:index, :create]
   skip_after_action :verify_authorized
 
   def index
@@ -14,12 +14,19 @@ class Api::V1::MessagesController < Api::V1::BaseController
       response = { message: 'Message created successfully' }
       render json: response, status: :created
     else
-      render json: @shop.errors, status: :bad
+      render json: @message.errors, status: :bad
     end
   end
 
+  private
+
   def message_params
     params.require(:message).permit(:ToUserName, :FromUserName, :CreateTime, :MsgType, :Content, :MsgId)
+  end
+
+  def render_error
+    render json: { errors: @message.errors.full_messages },
+      status: :unprocessable_entity
   end
 
 end
