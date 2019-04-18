24825e5cf190d21491ec19629537eedebd4fcd06
diego delrieu <diego.delrieu@gmail.com>
2019-04-18 19:03:41 +0800

    New features to event model

--- app/views/api/v1/events/index.json.jbuilder
+++ app/views/api/v1/events/index.json.jbuilder
@@ -1,5 +1,5 @@
 json.events do
   json.array! @events do |event|
-    json.extract! event, :id, :description, :timestamp
+    json.extract! event, :id, :description, :timestamp, :page, :customer_id, :item_id, :shop_id
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -9,16 +9,23 @@ Bundler.require(*Rails.groups)
 module ShopsApi
   class Application < Rails::Application
     config.generators do |generate|
-          generate.assets false
-          generate.helper false
-          generate.test_framework  :test_unit, fixture: false
-        end
+      generate.assets false
+      generate.helper false
+      generate.test_framework  :test_unit, fixture: false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
