bf6a8e4c855c2f92bbdadb72ae467f998fad8fa4
sophiafan <172404042@qq.com>
2019-04-19 08:18:47 +0800

    update schema

---
+++ db/migrate/20190418125755_add_userto_events.rb
@@ -0,0 +1,4 @@
+class AddUsertoEvents < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_04_18_113952) do
-
+ActiveRecord::Schema.define(version: 2019_04_18_125755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
